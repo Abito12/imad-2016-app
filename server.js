3f07421f03b4b7fc2386bcb9ec44b417f262c2a5
Abito12 <abitoprakashk@gmail.com>
2016-09-21 23:00:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.get('/ui/code-in-the-flow.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'code-in-the-flow.png'));
 });
 
+app.get('/ui/Lato-Light.ttf', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Lato-Light.ttf'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
