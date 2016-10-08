79a42cf5fc995f5993754af208d8452f1a75e39b
Abito12 <abitoprakashk@gmail.com>
2016-10-08 19:43:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+function NamePrompt() {
+    var Name, q = 'Enter Your Name';
+    n = window.sessionStorage.getItem('Name');
+    if (!n) {
+        n = window.prompt(q);
+        console.log(n);
+        window.sessionStorage.setItem('Name', n);
+    }
+    document.getElementById('username').innerHTML = n;
+    return n;
+}
