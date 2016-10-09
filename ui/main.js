605f8263c016d355682920837fcc693b31fd3a9d
Abito12 <abitoprakashk@gmail.com>
2016-10-10 01:18:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var getName= function(){
     return n;
 };
 
-//Counter Button
+/*Counter Button
 
 var button = document.getElementById('count-button');
 button.onclick = function(){
@@ -33,7 +33,9 @@ button.onclick = function(){
 	
 };
 
-console.log('main works')
+*/
+
+console.log('main works');
 // Comments Section
 
 var submit = document.getElementById('submitBtn');
