a80a12cfeff315624e5b5a0ab549a6d7d9cb199b
Abito12 <abitoprakashk@gmail.com>
2016-10-09 23:58:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+// Getting the Name of the User
 var getName= function(){
     var Name, q = 'Enter Your Name';
     n = window.sessionStorage.getItem('Name');
@@ -10,8 +11,9 @@ var getName= function(){
     return n;
 };
 
-var button = document.getElementById('count-button');
+//Counter Button
 
+var button = document.getElementById('count-button');
 button.onclick = function(){
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){
@@ -28,4 +30,30 @@ button.onclick = function(){
 	request.open('GET', "http://abito12.imad.hasura-app.io/counter", true);
 	request.send(null);
 	
+};
+
+
+// Comments Section
+
+var submit = document.getElementById('submit-btn');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var comment = '';
+                for(var i =0; i< comments.length;i++){
+                    comment = '<li>' + comments[i] + '</li>';
+                }
+                var ul = document.getElementById('comments-list');
+                ul.innerHTML = comment;
+            }
+        }
+    };
+    var comment = document.getElementById('comment-text');
+    comment = comment.value;
+    request.open('GET', 'http://abito12.imad.hasura-app.io/submit-cmnt/?comment='+comment, true);
+    request.send(null);
 };
