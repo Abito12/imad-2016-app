2bfb1b74f4f751dccc1f12bcd34dd2ef60f2da8f
Abito12 <abitoprakashk@gmail.com>
2016-10-11 15:38:19 +0530

    [imad-console] Updates ui/sub.js

--- ui/sub.js
+++ ui/sub.js
@@ -1,1 +1,29 @@
 
+
+var submitBtn = document.getElementById('submitBtn');
+submitBtn.onclick = function(){
+    
+    console.log('Clicked');
+    request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for (var i =0; i<comments.length; i++){
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var commentsList = document.getElementById('comments-list');
+                commentsList.innerHTML = list;
+            }
+        }
+    }
+    
+     
+    var commentBtn = document.getElementById('comment-text');
+    var comment = commentBtn.value;
+    request.open('GET', 'http://abito12.imad.hasura-app.io/submit?comment='+comment, true);
+    request.send(null);
+    
+}
