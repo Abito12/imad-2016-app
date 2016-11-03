6c761d5639308e7ea411cb606d888f2032174aad
Abito12 <abitoprakashk@gmail.com>
2016-11-03 15:17:19 +0530

    [imad-console] Updates ui/sub.js

--- ui/sub.js
+++ ui/sub.js
@@ -29,6 +29,8 @@ function getAllArticles(){
                 trans = "bottom";
                 color = "#FF69B4";
             }
+            
+            
             var Box = `<div class="box" style="background-color: ${color}">
                             <div class="cover ${trans}">
                                 <h2 class="title">${article.title}</h2>
