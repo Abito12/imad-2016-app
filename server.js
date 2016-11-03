18c4799d16737ee530ad2bf555f89fc9a4c39088
Abito12 <abitoprakashk@gmail.com>
2016-11-03 11:48:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/articles.html', function (req, res){
         if(err){
             res.status(500).send(err.toString());
         } else{
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
         }
     });
 });
