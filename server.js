896001ea569f3cfdf9f710b5117560d5c71abaa7
Abito12 <abitoprakashk@gmail.com>
2016-11-03 11:46:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'abito12',
+    database: 'abito12',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: 'process.env.DB_PASSWORD'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +17,29 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+//Articles Section
+
+
+var pool = new Pool(config);
+app.get('/articles.html', function (req, res){
+    pool.query('SELECT * FROM article', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+           res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
 
 //Comments Section
 var comments = [];
