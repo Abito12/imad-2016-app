12e3f4a1f3cc3db25e1632332f9903a719484856
Abito12 <abitoprakashk@gmail.com>
2016-11-03 14:25:11 +0530

    [imad-console] Updates ui/sub.js

--- ui/sub.js
+++ ui/sub.js
@@ -7,12 +7,20 @@ function getAllArticles(){
         console.log(allArticles);
         for(var i = 0;i < allArticles.length; i++){
             var article = allArticles[i];
+            var trans;
+            
+            if(i % 2 === 0){
+                trans = "left";
+            }
+            if(i % 2 === 1){
+                trans ="right";
+            }
             var Box = `<div class="box">
-                            <div class="cover left">
+                            <div class="cover ${trans}">
                                 <h2 class="title">${article.title}</h2>
                                 <p class="intro">${article.content}</p>
                             <div class="btn"><a href="#">Read more...</a></div>
-                            <p class="date">${article.date.}.toString()</p>
+                            <p class="date"></p>
                             </div>
                     </div>`;
             $('.container').append(Box);
