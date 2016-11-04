b584f3b42ff149c4048827856607b64a8bf45051
Abito12 <abitoprakashk@gmail.com>
2016-11-05 02:21:48 +0530

    [imad-console] Updates ui/sub.js

--- ui/sub.js
+++ ui/sub.js
@@ -7,19 +7,17 @@ function getAllArticles(){
         console.log(allArticles);
         for(var i = 0;i < allArticles.length; i++){
             var article = allArticles[i];
-            var trans = ["left", "right","top","bottom","bottom-right","top-left"];
-            var color = ["#3399FF", "#CC0033", "#03A8A6" , "#6633CC", "#009933", "#E25422"];
-            var rand = i % 6;
-            
-            var Box = `<div class="box" style="background-color: ${color[rand]}">
-                            <div class="cover ${trans[rand]}">
-                                <h2 class="title">${article.title}</h2>
-                                <p class="intro">${article.content}</p>
-                                <div class="btn"><a href="#">Read more...</a></div>
-                                <p class="date">${article.date}</p>
+            var Box = `<div class="col-md-6 item">
+                            <div class="item-in">
+                                <h4>${article.title}</h4>
+                                <div class="seperator"></div>
+                                <p>${article.content}</p>
+                                <a href="#">Read More
+                                <i class="fa fa-long-arrow-right"></i>
+                                </a>
                             </div>
-                    </div>`;
-            $('.container').append(Box);
+                        </div>`;
+            $('.articleBox').append(Box);
             
         }
     }
