489270e6643934cd2f58f1ca5d697cae2a1a1dfc
Abito12 <abitoprakashk@gmail.com>
2016-11-06 18:38:16 +0530

    [imad-console] Updates ui/articlePage.js

--- ui/articlePage.js
+++ ui/articlePage.js
@@ -1,10 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title> Article Two </title>
-</head>
-<body>
-    <h1>Article Two</h1>
-</body>
-</html>
 
+function getAArticle(){
+    $.ajax({
+    url: "/Articled",
+    success: function(result){
+        var allArticles = JSON.parse(result);
+        console.log(allArticles);
+        for(var i = 0;i < allArticles.length; i++){
+            var article = allArticles[i];
+            var Box = `<div class="col-md-6 item">
+                            <div class="item-in">
+                                <h4>${article.title}</h4>
+                                <div class="seperator"></div>
+                                <p>${article.content}</p>
+                                <a href="#">Read More
+                                <i class="fa fa-long-arrow-right"></i>
+                                </a>
+                            </div>
+                        </div>`;
+            $('#articleBox').append(Box);
+            
+        }
+    }
+});
+}
+
+$(document).ready(function(){
+    getAllArticles();
+});
