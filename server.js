a1195be27c58d26f961abec1d20c1e28d65b8a26
Abito12 <abitoprakashk@gmail.com>
2016-11-25 10:15:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+const crypto = require('crypto');
 
 
 // Database Credentials
@@ -15,6 +16,9 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -38,6 +42,17 @@ app.get('/about.html', function (req, res) {
 
 app.get('/ui/about.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.css'));
+});
+
+//Login Functionality
+function hash(input, salt){
+    var hashed = crypto.pbkdf2Sync(input  , salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString); 
 });
 
 
