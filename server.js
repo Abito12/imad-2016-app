fdc9895201ccc7d916013d92c1661f7dd885f122
Abito12 <abitoprakashk@gmail.com>
2016-11-25 09:51:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-const crypto = require('crypto');
+
 
 // Database Credentials
 var config = {
@@ -15,8 +15,6 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -41,37 +39,6 @@ app.get('/about.html', function (req, res) {
 app.get('/ui/about.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.css'));
 });
-
-//Login Functionality
-
-function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input  , salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
-}
-
-app.get('/hash/:input', function(req, res){
-    var hashedString = hash(req.params.input,'this-is-some-random-string');
-    res.send(hashedString); 
-});
-
-
-app.post('/create-user', function(req, res){
-    var username = req.body.username;
-    var password = req.body.password;
-    var salt = crypto.getRandomBytes(128).toString('hex');
-    var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)', [username, dbString], function(err, result){
-            if(err){
-                res.status(500).send(err.toString());
-            }
-            else{
-            res.send('User Successfully added' + username);  
-            }
-    });
-});
-
-
-
 
 
 //Articles Main Page
