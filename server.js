247ad846d7930c402073cd85ac76dc3fef3ccdb2
Abito12 <abitoprakashk@gmail.com>
2016-11-25 19:11:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/check-login', function(req, res){
 
 app.get('/logout', function(req, res){
     delete req.session.auth;
-    res.send('Logged out');
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
