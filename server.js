a34cd42eee8e52b7082134c2a73adac4410260f9
Abito12 <abitoprakashk@gmail.com>
2016-11-25 20:09:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,42 +161,6 @@ function createArticleTemplate(data){
   </script>
 </head>
 <body>
-<div class="Headcontainer">
-<div class="left_el">Home</div>
-<div class="left_el" onclick="window.location.href='http://abito12.imad.hasura-app.io/about'">About Me</div>
-<div class="left_el">Contact</div>
-<div class="header">
-  <div class="inner">
-    <div class="user-ui">
-
-      <div class="user-menu-toggle">
-        <div class="profile-img" style="background-image:url(https://s3.amazonaws.com/uifaces/faces/twitter/mlane/128.jpg)"></div>
-        <span class="simple-arrow fa fa-chevron-up"></span>
-      </div>
-      <div class="user-menu">
-
-        <div class="user-info">
-
-          <div class="profile-img"  style="background-image:url(https://s3.amazonaws.com/uifaces/faces/twitter/mlane/128.jpg)"></div>
-          <h3 class="name">User Name</h3>
-          <div class="ui btn normal">Profile</div>
-        </div>
-
-        <div class="menu-nav">
-          <li><span class="fa fa-question"></span>Dashboard</li>
-          <li onclick="window.location.href='http://abito12.imad.hasura-app.io/logout'"><span class="fa fa-power-off"></span>Sign Out</li>
-        </div>
-      </div>
-
-      <div class="sign-out-icon">
-        <span class="fa fa-power-off"></span>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://raw.githubusercontent.com/julianshapiro/velocity/master/velocity.min.js'></script>
 
 <div id="container">
 <div class="blog">
