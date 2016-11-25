75abdaca024fde016d079df41ff85524cb5148ba
Abito12 <abitoprakashk@gmail.com>
2016-11-25 18:40:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,11 @@ app.get('/check-login', function(req, res){
    }
 });
 
+app.get('/logout', function(req, res){
+    delete req.session.auth;
+    res.send('Logged out');
+});
+
 
 //Articles Main Page
 
