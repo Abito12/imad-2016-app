ebb6469fdfb09feefbd716d634bfeca49afec552
Abito12 <abitoprakashk@gmail.com>
2016-11-25 19:50:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,8 @@ submit.onclick = function(){
   request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
-              alert('Login is successfull');
+              window.location.href='http://abito12.imad.hasura-app.io/about';
+              console.log("Successful Login");
           }
           else if(request.status === 403){
               alert('Incorrect username and password');
@@ -49,6 +50,36 @@ submit.onclick = function(){
   
 };
 
+
+
+var signup = document.getElementById('signup-btn');
+submit.onclick = function(){
+  var request = new XMLHttpRequest();
+  
+  
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              alert('Login is successful');
+          }
+          else if(request.status === 403){
+              alert('Incorrect username and password');
+          }
+          else if(request.status ===503){
+              alert('Internal Error');
+          }
+      }
+  };
+  
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log('password');
+  request.open('POST', 'http://abito12.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password: password}));
+  
+};
 
 
 
