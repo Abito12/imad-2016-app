872713829165a3e002162f67e25fcde115954eb6
Abito12 <abitoprakashk@gmail.com>
2016-11-26 00:52:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,11 +104,9 @@ app.post('/login', function(req, res){
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString){
                     //Set the session
-                    
                     req.session.auth = {userId: result.rows[0].id};
-                    
                     res.send('Credentials Correct');
-                } else {
+                } else if(hashedPassword !== dbString){
                     res.send(403).send('Invalid Login');
                 }
             }
