8a0e109537e057334fc44d853d96acb4dc77eea2
Abito12 <abitoprakashk@gmail.com>
2016-11-26 18:37:14 +0530

    [imad-console] Updates ui/newarticle.js

--- ui/newarticle.js
+++ ui/newarticle.js
@@ -2,33 +2,8 @@ $(document).ready(function(){
 
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
-      var request = new XMLHttpRequest();
-  
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 500){
-              alert('Article could not be added');
-          }
-          else {
-              alert('Article was created');
-              window.location.href='http://abito12.imad.hasura-app.io/articles';
-          }
-      }
-  };
-  
-  var title = document.getElementById('title').value;
-  var content = document.getElementById('content').value;
-  console.log(content);
-  console.log(title);
-  request.open('POST', 'http://abito12.imad.hasura-app.io/add-article' + id , true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify({title:title, content:content}));
-  
-
-
-
+    alert('Hey npw brown cow');
 };
-
 });
 
 
