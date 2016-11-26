4495bfb0ae0fe71354666b011b453818a082a5dd
Abito12 <abitoprakashk@gmail.com>
2016-11-26 11:07:40 +0530

    [imad-console] Updates ui/articlePage.js

--- ui/articlePage.js
+++ ui/articlePage.js
@@ -1,27 +1,11 @@
 
 function getAllComments(){
-    $.ajax({
-    url: "/articles/comments",
-    success: function(result){
-        var allComments = JSON.parse(result);
-        console.log(allComments);
-        for(var i = 0;i < allComments.length; i++){
-            var Comment = allComments[i];
-            var Box = `<li>
-                        <div class="commentText">
-                            <p class="">${Comment.comment}</p> <span class="date sub-text">${Comment.author}</span>
-                        </div>
-                    </li>`;
-            $('.commentslist').append(Box);
-            
-        }
-    }
-});
+        var key = document.getElementById('key').innerHTML.toString();
+console.log('script works');
+console.log(key);
 }
 
 $(document).ready(function(){
     getAllComments();
-    var key = document.getElementById('key').innerHTML.toString();
-console.log('script works');
-console.log(key);
+
 });
