654c0281127b449dd5f867de1e489246584e8527
Abito12 <abitoprakashk@gmail.com>
2016-11-28 01:59:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -550,16 +550,15 @@ app.get('/editArticle/:articleID', function(req, res){
             res.status(404).send('Article Cannot be Edited');
         } else{
             var articleData = result.rows[0];
-            if(articleData.author_id === req.session.auth.userId){
-                if(req.session && req.session.auth && req.session.auth.userId){
-                res.send(createArticleTemplate2(articleData));
+            if(req.session && req.session.auth && req.session.auth.userId){
+            res.send(createArticleTemplate2(articleData));
             }else{
                   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
             }
             }
     });
 });
-}
+
 
 
 
