19dc7759bc2c64fd9b0f562cbce938bd80a84994
Abito12 <abitoprakashk@gmail.com>
2016-11-27 16:26:57 +0530

    [imad-console] Updates ui/newarticle.js

--- ui/newarticle.js
+++ ui/newarticle.js
@@ -18,13 +18,24 @@ submit.onclick = function(){
   
   var title = document.getElementById('title').value;
   var content = document.getElementById('content').value;
+  
+  if(title.length ===0){
+      alert('Please provide a title');
+  }
+  else if(title.length > 30){
+      alert('Maximum 30 letters for title');
+  }
+  else if(content.length === 0){
+      alert('Content Section is Empty');
+  }
+  else{
   content = content.replace(/(?:\r\n|\r|\n)/g, '<br />');
   console.log(content);
   console.log(title);
   request.open('POST', 'http://abito12.imad.hasura-app.io/add-article', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({title:title, content:content}));
-  
+  }
 
 
 
