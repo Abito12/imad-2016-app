16ba3c9f82891b326e4ef08b1c1039b8f28a3fff
Abito12 <abitoprakashk@gmail.com>
2016-11-28 02:11:01 +0530

    [imad-console] Updates ui/sub.js

--- ui/sub.js
+++ ui/sub.js
@@ -88,7 +88,7 @@ function getMyArticles(){
                                 <a href = "http://abito12.imad.hasura-app.io/articles/${article.id}">Read More
                                 <i class="fa fa-long-arrow-right"></i>
                                 </a>
-                                 <a id="edit-link" href = "http://abito12.imad.hasura-app.io/articles/${article.id}">Read More
+                                 <a id="edit-link" href = "http://abito12.imad.hasura-app.io/editArticle/${article.id}">Read More
                                 <i class="fa fa-long-arrow-right"></i>
                                 </a>
                             </div>
