d962bd639467be60fcdbf3c3feaa1b2bae18296d
Abito12 <abitoprakashk@gmail.com>
2016-11-28 22:46:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -299,7 +299,7 @@ app.get('/articles/:articleID', function(req, res){
 
 app.get('/comments/:articleID', function(req, res){
     
-    pool.query("SELECT article_comments.body,user_info.username FROM article_comments,user_info WHERE article_comments.user_id = user_info.id AND article_comments.article_id = " + req.params.articleID, function(err, result){
+    pool.query("SELECT article_comments.date, article_comments.body, user_info.username FROM article_comments,user_info WHERE article_comments.user_id = user_info.id AND article_comments.article_id = " + req.params.articleID, function(err, result){
         if(err){
             res.status(500).send(err.toString());
         } else if(result.rows.length === 0){
