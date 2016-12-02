02ea08d8f6c68b59ceda7c4381c66616818bd472
Abito12 <abitoprakashk@gmail.com>
2016-12-02 23:46:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -428,8 +428,8 @@ app.post('/add-article', function(req, res){
     var str1 = title.trim();
     var str2 = content.trim();
     var regex3 = /[^a-z-^0-9,#!@#$%&*()*._+=/]/gi;
-    var str3 = title.replace(regex3, "");
-    var str4 = content.replace(regex3,"");
+    var str3 = title.replace(regex3, " ");
+    var str4 = content.replace(regex3," ");
     console.log(str4);
     console.log(content);
     if(title.length === 0 || content.length === 0 || str1.length === 0 || str2.length ===0 || str3.length != title.length || str4.length != content.length){
