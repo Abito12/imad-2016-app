d5729066932237967e6214bca66ce8c214307ef6
Abito12 <abitoprakashk@gmail.com>
2016-12-09 20:21:36 +0530

    [imad-console] Updates ui/profile.js

--- ui/profile.js
+++ ui/profile.js
@@ -13,4 +13,36 @@ function getUsername(){
 
 $(document).ready(function(){
     getUsername();
+    var submit = document.getElementById('contact-send');
+    submit.onclick = function(){
+    var request = new XMLHttpRequest();
+     request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 500){
+              alert('Could not update user profile');
+          }
+          else {
+              alert('Profile Updated');
+              window.location.href='http://abito12.imad.hasura-app.io/articles';
+          }
+      }
+  };
+  
+  var info = document.getElementById('userinfo').value;
+  var email = document.getElementById('useremail').value;
+  var bio = document.getElementById('userbio').value;
+  
+  if(info.length ===0 || email.length === 0 || bio.length ===0){
+      alert('Please fill out all the fields');
+  }
+ 
+  else{
+  request.open('POST', 'http://abito12.imad.hasura-app.io/update-profile', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({info:info, email:email, bio: bio}));
+  }
+
+
+
+};
 });
