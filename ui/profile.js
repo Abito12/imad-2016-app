3a45fa4df2c3509395eea654d4682e0249885921
Abito12 <abitoprakashk@gmail.com>
2016-12-09 16:12:05 +0530

    [imad-console] Updates ui/profile.js

--- ui/profile.js
+++ ui/profile.js
@@ -1,1 +1,12 @@
+function getUsername(){
+    $.ajax({
+        url: "/check-login",
+        success: function(result){
+            var userDetails = JSON.parse(result);
+            console.log(userDetails);
+            var name = userDetails[0].username;
+            $('#username').html(name); 
+        }
+    });
+}
 
