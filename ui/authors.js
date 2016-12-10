3549190cdbabcf9d613827ef4f9226e6d5c7e56f
Abito12 <abitoprakashk@gmail.com>
2016-12-10 20:51:45 +0530

    [imad-console] Updates ui/authors.js

--- ui/authors.js
+++ ui/authors.js
@@ -1,1 +1,27 @@
-
+function getAuthors(){
+    $.ajax({
+    url: "/allAuthors",
+    success: function(result){
+        $('#mainbox').html("");
+        var allAuthors = JSON.parse(result);
+        var countrows = Math.ciel(allAuthors.length/3);
+        for(var i=0; i < countrows; i++){
+            var  rows = `<div  class="row">`;
+            var fourcolumns = "";
+            for(var j = 0; j < 3; j++){
+                author = allAuthors[i * 3 + j];
+                fourcolumns += `<div class="four columns">
+                    <div class="teaser">
+                    <h3>${author.username}</h3>
+                    <h6><span>${author.profession}</span></h6>
+                    <p>${author.bio}</p>
+                    <h6><a href = "#">Show Articles</a></h6>
+                    </div>
+                    </div>`;
+            }
+            rows = rows + fourcolumns + `</div>`;
+            $('#mainbox').append(rows);
+        }
+    }
+});
+}
