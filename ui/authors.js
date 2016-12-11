d9e11c0ade8a5f2c425251df6c8c3e1504855cb3
Abito12 <abitoprakashk@gmail.com>
2016-12-11 16:24:32 +0530

    [imad-console] Updates ui/authors.js

--- ui/authors.js
+++ ui/authors.js
@@ -5,15 +5,11 @@ function getAuthors(){
         $('#mainbox').append("<h1>Article Authors</h1>");
         var allAuthors = JSON.parse(result);
         var countrows = Math.ceil(allAuthors.length/3);
-        console.log(countrows);
         for(var i=0; i < countrows; i++){
-            console.log('first loop');
             var  rows = `<div  class="row">`;
             var fourcolumns = "";
             for(var j = 0; j < 3; j++){
-                console.log('second');
                 var k = i*3+j;
-                console.log(k);
                 if(k<allAuthors.length){
                 author = allAuthors[k];
                 fourcolumns += `<div class="four columns">
