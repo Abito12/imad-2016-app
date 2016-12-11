80ab1730183dc615ab520b379d348b290aeac9bd
Abito12 <abitoprakashk@gmail.com>
2016-12-11 13:16:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.get('/check-login', function(req, res){
 //Author Info
 app.get('/allAuthors', function (req, res) {
     
-    pool.query("SELECT DISTINCT user_info.username, user_info.profession, user_info.bio FROM user_info, article WHERE user_info.id = article.author_id", function(err, result){
+    pool.query("SELECT DISTINCT user_info.username, user_info.profession, user_info.bio FROM user_info, article WHERE user_info.id = article.author_id ORDER BY user_info.bio", function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
